b432a7f9b216a9b9d2f372d0e302c6589cba6d1d
David Steinich <drsteini@contractor.usgs.gov>
2020-02-10 11:09:02 -0600

    moer logging

--- src/main/java/gov/usgs/wma/waterdata/PreProcess.java
+++ src/main/java/gov/usgs/wma/waterdata/PreProcess.java
@@ -23,17 +23,29 @@ public class PreProcess implements Function<RequestObject, ResultObject> {
 	@Override
 	@Transactional
 	public ResultObject apply(RequestObject requestObject) {
-		LOG.debug("json_data_id: {}", requestObject.getId());
+		LOG.info("json_data_id: {}", requestObject.getId());
 		ResultObject resultObject = new ResultObject();
 		try {
 			resultObject.setUniqueId(jsonDataDao.getUniqueId(requestObject.getId()));
-			LOG.debug("guid: {}", resultObject.getUniqueId());
+			LOG.info("guid: {}", resultObject.getUniqueId());
+			LOG.info("Before Approvals");
 			jsonDataDao.doApprovals(requestObject.getId());
+			LOG.info("After Approvals");
+			LOG.info("Before GapTolerances");
 			jsonDataDao.doGapTolerances(requestObject.getId());
+			LOG.info("After GapTolerances");
+			LOG.info("Before Grades");
 			jsonDataDao.doGrades(requestObject.getId());
+			LOG.info("After Grades");
+			LOG.info("Before InterpolationTypes");
 			jsonDataDao.doInterpolationTypes(requestObject.getId());
+			LOG.info("After InterpolationTypes");
+			LOG.info("Before Methods");
 			jsonDataDao.doMethods(requestObject.getId());
+			LOG.info("After Methods");
+			LOG.info("Before Points");
 			jsonDataDao.doPoints(requestObject.getId());
+			LOG.info("After Points");
 		} catch (IOException e) {
 			LOG.error(e.getLocalizedMessage());
 		}
