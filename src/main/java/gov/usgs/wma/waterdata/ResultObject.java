2f1affa56f154a3591fbf22c06cb38ebdfa0a13c
David Steinich <drsteini@contractor.usgs.gov>
2020-02-25 06:17:21 -0600

    Another attempt at correct json

--- src/main/java/gov/usgs/wma/waterdata/ResultObject.java
+++ src/main/java/gov/usgs/wma/waterdata/ResultObject.java
@@ -3,7 +3,10 @@ package gov.usgs.wma.waterdata;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ResultObject {
+	@JsonProperty("timeSeries")
 	private List<TimeSeries> timeSeriesList;
 	public List<TimeSeries> getTimeSeriesList() {
 		return null != timeSeriesList ? timeSeriesList : new ArrayList<TimeSeries>();
