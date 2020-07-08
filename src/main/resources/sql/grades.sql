0304e71759200bf1fec079a8b701eaffade70b2e
ssoper <ssoper@usgs.gov>
2020-07-08 09:17:00 -0500

    grades tests are passing with addition of partition number

--- src/main/resources/sql/grades.sql
+++ src/main/resources/sql/grades.sql
@@ -2,13 +2,17 @@ insert
   into time_series_grades (json_data_id,
                            start_time,
                            end_time,
-                           grade_code
+                           grade_code,
+                           partition_number
                           )
 select json_data_id,
        adjust_timestamp(jsonb_extract_path_text(grades, 'StartTime')) start_time,
        adjust_timestamp(jsonb_extract_path_text(grades, 'EndTime')) end_time,
-       jsonb_extract_path_text(grades, 'GradeCode') grade_code
+       jsonb_extract_path_text(grades, 'GradeCode') grade_code,
+       partition_number
   from (select json_data_id,
-               jsonb_array_elements(jsonb_extract_path(json_content, 'Grades')) grades
+               jsonb_array_elements(jsonb_extract_path(json_content, 'Grades')) grades,
+               partition_number
           from json_data
-         where json_data_id = ?) a
+         where json_data_id = ?
+           and partition_number = ?) a
